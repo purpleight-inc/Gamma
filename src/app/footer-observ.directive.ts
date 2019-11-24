cf7a619da56cbaaeb889999554657e076fe4993f
imadhen <imaddetreize@gmail.com>
2019-11-24 13:28:42 +0100

    fixing the instersection observers to observe the footer on sidenav

--- src/app/footer-observ.directive.ts
+++ src/app/footer-observ.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef } from '@angular/core';
+import { query } from '@angular/animations';
 
 @Directive({
   selector: '[appFooterObserv]'
@@ -9,13 +10,18 @@ export class FooterObservDirective {
   constructor(public elem: ElementRef) {
    }
   ngAfterViewInit(){
-    const  options = {};
+    const  options = {
+      root : null,
+      treshhold : 0,
+      rootMargin : "-200px"
+    };
     const observer = new IntersectionObserver(function(entries,observer){
       entries.forEach(entry => {
-        console.log(entry)
+        console.log(entry.isIntersecting);
       });
     }, options);
-    
+
     observer.observe(this.elem.nativeElement);
+    
   }
 }
